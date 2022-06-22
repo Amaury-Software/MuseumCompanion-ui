03a57601d0876276f6e22623a20d80a6057fa989
danis <amaury.danis-cousandier@epitech.eu>
2022-06-22 06:40:39 +0200

    #3 splashscreen created

--- src/views/SplashScreen.tsx
+++ src/views/SplashScreen.tsx
@@ -1,12 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity } from 'react-native';
 
 export function SplashScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar  style="auto" animated={true}/>
-    </View>
+    <SafeAreaView style={styles.container}>
+
+      <Text style={styles.title}>Museum Companion</Text>
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity>
+        <Text  style={styles.accountText}>Don't have an account yet ?</Text>
+      </TouchableOpacity>
+
+    </SafeAreaView>
     );
 }
 
@@ -17,4 +26,23 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 30
+  },
+  button: {
+    marginTop: '20%',
+    borderRadius: 20,
+    backgroundColor: '#1098ad',
+  },
+  buttonText: {
+    fontWeight: '800',
+    fontSize: 20,
+    color: 'white',
+    margin: 15
+  },
+  accountText: {
+    marginTop: 10,
+    color: 'gray',
+  }
 });
